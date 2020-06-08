b0b8295a3eab38075b2097445552035766d9088d
Gustavo Ciotto <gustavociotto@gmail.com>
2020-06-08 08:25:58 -0300

    COMMON: throw exception if file couldn't be opened.

--- common/src/output/Dumper.cpp
+++ common/src/output/Dumper.cpp
@@ -26,6 +26,8 @@ Dumper::Dumper(const string& path, const string& dirname) {
 }
 
 void Dumper::createDir() const {
+    LOGI("Creating output directory at " << outputDirectoryPath);
+
     if (!boost::filesystem::create_directory(outputDirectoryPath)) {
         throw invalid_argument("Directory couldn't be created");
     }
@@ -33,95 +35,113 @@ void Dumper::createDir() const {
 
 void Dumper::dumpAlgorithm(ComputeAlgorithm* algorithm) const {
     ofstream algorithmOutputFile;
-    algorithmOutputFile.open(outputDirectoryPath + "algorithm.txt");
 
-    if (algorithmOutputFile.is_open()) {
+    const string algorithmFile = outputDirectoryPath + "algorithm.txt";
 
-        algorithmOutputFile << "==== Compute Algorithm Summary ====" << endl;
-        algorithmOutputFile << algorithm->toString() << endl;
+    algorithmOutputFile.open(algorithmFile);
 
-        algorithmOutputFile.close();
+    if (!algorithmOutputFile.is_open()) {
+        throw runtime_error("Couldn't open file " + algorithmFile + "to write algorithm parameters");
     }
+
+    algorithmOutputFile << "==== Compute Algorithm Summary ====" << endl;
+    algorithmOutputFile << algorithm->toString() << endl;
+
+    algorithmOutputFile.close();
 }
 
 void Dumper::dumpGatherParameters(const string& file) const {
     Gather* gather = Gather::getInstance();
 
+    const string gatherFile = outputDirectoryPath + "gather.txt";
+
     ofstream gatherOutputFile;
-    gatherOutputFile.open(outputDirectoryPath + "gather.txt");
+    gatherOutputFile.open(gatherFile);
 
-    if (gatherOutputFile.is_open()) {
-
-        gatherOutputFile << "==== Gather Summary ====" << endl;
-        gatherOutputFile << "Input file = " << file << endl;
-        gatherOutputFile << gather->toString() << endl;
-
-        gatherOutputFile.close();
+    if (!gatherOutputFile.is_open()) {
+        throw runtime_error("Couldn't open file " + gatherFile + " to write gather parameters");
     }
+
+    gatherOutputFile << "==== Gather Summary ====" << endl;
+    gatherOutputFile << "Input file = " << file << endl;
+    gatherOutputFile << gather->toString() << endl;
+
+    gatherOutputFile.close();
 }
 
 void Dumper::dumpResult(const string& resultName, const MidpointResult& result) const {
     Gather* gather = Gather::getInstance();
 
+    const string resultFile = outputDirectoryPath + resultName + ".su";
+
     ofstream resultOutputFile;
-    resultOutputFile.open(outputDirectoryPath + resultName + ".su");
+    resultOutputFile.open(resultFile);
 
-    if (resultOutputFile.is_open()) {
-
-        for (auto it = gather->getCdps().begin(); it != gather->getCdps().end(); it++) {
-
-            float m0 = it->first;
-            const Cdp& cdp = it->second;
-
-            const vector<float>& samples = result.get(m0);
-
-            trace_info_t cdpInfo = cdp.getCdpInfo();
-
-            resultOutputFile.write(
-                reinterpret_cast<const char*>(&cdpInfo),
-                sizeof(trace_info_t)
-            );
-
-            resultOutputFile.write(
-                reinterpret_cast<const char*>(samples.data()),
-                samples.size() * sizeof(float)
-            );
-        }
-
-        resultOutputFile.close();
+    if (!resultOutputFile.is_open()) {
+        throw runtime_error("Couldn't open " + resultFile + " file to write result data.");
     }
+
+    for (auto it = gather->getCdps().begin(); it != gather->getCdps().end(); it++) {
+
+        float m0 = it->first;
+        const Cdp& cdp = it->second;
+
+        const vector<float>& samples = result.get(m0);
+
+        trace_info_t cdpInfo = cdp.getCdpInfo();
+
+        LOGI("Writing " << sizeof(trace_info_t) << " bytes related to header.");
+
+        resultOutputFile.write(
+            reinterpret_cast<const char*>(&cdpInfo),
+            sizeof(trace_info_t)
+        );
+
+        LOGI("Writing " << samples.size() * sizeof(float) << " bytes related to data.");
+
+        resultOutputFile.write(
+            reinterpret_cast<const char*>(samples.data()),
+            samples.size() * sizeof(float)
+        );
+    }
+
+    resultOutputFile.close();
 }
 
 void Dumper::dumpStatisticalResult(const string& statResultName, const StatisticalMidpointResult& statResult) const {
     Gather* gather = Gather::getInstance();
 
+    const string resultFile = outputDirectoryPath + statResultName + ".csv";
+
     ofstream resultOutputFile;
-    resultOutputFile.open(outputDirectoryPath + statResultName + ".csv");
+    resultOutputFile.open(resultFile);
 
-    if (resultOutputFile.is_open()) {
-
-        for (auto it = gather->getCdps().begin(); it != gather->getCdps().end(); it++) {
-
-            float m0 = it->first;
-
-            resultOutputFile << m0 << "," << statResult.getStatisticalResultForMidpoint(m0) << endl;
-        }
-
-        resultOutputFile.close();
+    if (!resultOutputFile.is_open()) {
+        throw runtime_error("Couldn't open " + resultFile + " file to write statistical result data.");
     }
+
+    for (auto it = gather->getCdps().begin(); it != gather->getCdps().end(); it++) {
+        float m0 = it->first;
+        resultOutputFile << m0 << "," << statResult.getStatisticalResultForMidpoint(m0) << endl;
+    }
+
+    resultOutputFile.close();
 }
 
 void Dumper::dumptraveltime(Traveltime* model) const {
+    const string traveltimeFile = outputDirectoryPath + "traveltime.txt";
+
     ofstream modelOutputFile;
-    modelOutputFile.open(outputDirectoryPath + "traveltime.txt");
+    modelOutputFile.open(traveltimeFile);
 
-    if (modelOutputFile.is_open()) {
-
-        modelOutputFile << "==== Model Summary ====" << endl;
-        modelOutputFile << model->toString() << endl;
-
-        modelOutputFile.close();
+    if (!modelOutputFile.is_open()) {
+        throw runtime_error("Couldn't open " + traveltimeFile + " to write traveltime model parameters.");
     }
+
+    modelOutputFile << "==== Model Summary ====" << endl;
+    modelOutputFile << model->toString() << endl;
+
+    modelOutputFile.close();
 }
 
 
