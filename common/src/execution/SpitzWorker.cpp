df29000e3b71818d1438efeaabec1588cad04755
Otavio Napoli <otavio.napoli@gmail.com>
2020-10-06 16:47:24 -0300

    Added metric

--- common/src/execution/SpitzWorker.cpp
+++ common/src/execution/SpitzWorker.cpp
@@ -1,5 +1,6 @@
 #include <chrono>
 #include "common/include/execution/SpitzWorker.hpp"
+#include "common/include/semblance/result/StatisticResult.hpp"
 
 using namespace std;
 
@@ -19,23 +20,16 @@ int SpitzWorker::run(spits::istream& task, const spits::pusher& result) {
         computeAlgorithm->setUp();
     }
 
-    auto start = chrono::high_resolution_clock::now();
-    
     computeAlgorithm->computeSemblanceAndParametersForMidpoint(m0);
+
     const vector<float>& semblanceResults = computeAlgorithm->getComputedResults();
-    
-    auto end = chrono::high_resolution_clock::now();
-    double time_taken = chrono::duration_cast<chrono::nanoseconds>(end - start).count(); 
-    metrics.add_metric("semblance_time", time_taken);
-    
-
     outputStream.write_float(m0);
-
     outputStream.write_data(semblanceResults.data(), semblanceResults.size() * sizeof(float));
 
     for (unsigned int i = 0; i < static_cast<unsigned int>(StatisticResult::CNT); i++) {
         StatisticResult statResult = static_cast<StatisticResult>(i);
         outputStream.write_float(computeAlgorithm->getStatisticalResult(statResult));
+        metrics.add_metric(STATISTIC_NAME_MAP[statResult], computeAlgorithm->getStatisticalResult(statResult));
     }
 
     result.push(outputStream);
